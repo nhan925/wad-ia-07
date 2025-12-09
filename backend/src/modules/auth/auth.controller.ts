7d3e6d70ef8664ac962f79b1d2d06c37d78ae990
Nguyen Trong Nhan <info.nhan925@gmail.com>
2025-12-09 11:00:20 +0700

    change same-site to lax instead of none

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -58,7 +58,7 @@ export class AuthController {
     response.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', // HTTPS in production
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // 'none' for cross-site in production
+      sameSite: 'lax', // 'none' for cross-site in production
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
